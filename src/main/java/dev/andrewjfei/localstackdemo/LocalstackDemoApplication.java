81fd6885fa1c09e6fce0ab7806c01c7edb1faa8b
Andrew J Fei <andrewjfei@gmail.com>
2022-09-06 11:53:18 +1200

    NONE - init project

---
+++ src/main/java/dev/andrewjfei/localstackdemo/LocalstackDemoApplication.java
@@ -0,0 +1,13 @@
+package dev.andrewjfei.localstackdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalstackDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalstackDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/andrewjfei/localstackdemo/LocalstackDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.andrewjfei.localstackdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalstackDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
